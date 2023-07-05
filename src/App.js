7256d3fbba10bf4f3e44465fc2ca4358d50cfdfa
ROHIT KHARWAR <rohitkumar27699@gmail.com>
2023-07-05 23:51:22 +0530

    branch Created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Heading } from '@chakra-ui/react';
 import { useSelector } from 'react-redux';
 import "./App.css";
+import { Cart } from './components/Cart';
 function App() {
   const {name}=useSelector((state)=>state);
 console.log(name);
   return (
     <div className="App">
     <Heading> {name} React Project</Heading>
-    </div>
+    <Cart/>
+   </div>
   );
 }
 
